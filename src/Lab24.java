aaaa9088ecdb2d91a6c1ce299073a10cf0eae4de
deepti88 <deeptivt@gmail.com>
2024-01-07 21:20:30 +0530

    done

---
+++ src/Lab24.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Lab24 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder=new StringBuilder("Deepti");
+
+        stringBuilder.reverse();
+
+        System.out.println(stringBuilder);
+    }
+}
