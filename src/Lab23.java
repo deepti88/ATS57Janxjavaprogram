5b398f9db244fafbc489a49b6ce09286255fe950
deepti88 <deeptivt@gmail.com>
2024-01-07 20:54:36 +0530

    done

---
+++ src/Lab23.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Lab23 {
+    public static void main(String[] args) {
+        String name="Deepti";
+        String name2=new String("Vashisht");
+
+        String str="Deepti";
+        String str2=str.concat(" Vashisht");
+        String str3=str;
+
+        System.out.println(str2);
+      System.out.println(str);
+        System.out.println(str3);
+    }
+}
